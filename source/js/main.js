f2c23e248b0f8fd5a5049fc2fe45abe2e3ff0f4c
Andrew <nk-andrew1987@mail.ru>
2020-03-08 21:27:02 +0300

    Начат блок с табами

--- source/js/main.js
+++ source/js/main.js
@@ -2,5 +2,15 @@
 
 document.addEventListener('DOMContentLoaded', function () {
 
+  var sliderIntro = new Swiper('.intro', {
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    },
+    autoplay: {
+      delay: 4000,
+    },
+    speed: 700
+  });
 
 });
