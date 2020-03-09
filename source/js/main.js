0f4337fa7eb2dbf97070efa0c74d117147df13de
Andrew <nk-andrew1987@mail.ru>
2020-03-09 21:09:00 +0300

    Практически доделан блок услуги

--- source/js/main.js
+++ source/js/main.js
@@ -2,15 +2,88 @@
 
 document.addEventListener('DOMContentLoaded', function () {
 
-  var sliderIntro = new Swiper('.intro', {
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true
-    },
-    autoplay: {
-      delay: 4000,
-    },
-    speed: 700
+  var tabs = document.querySelectorAll('.services__tab-item');
+  var tabContents = document.querySelectorAll('.services__block');
+  var introPage = document.querySelector('.intro');
+  var servicesPage = document.querySelector('.services');
+
+  // СЛайдер
+
+  if (introPage) {
+    var sliderIntro = new Swiper('.intro', {
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+      },
+      autoplay: {
+        delay: 4000,
+      },
+      speed: 700
+    });
+
+    sliderIntro.init();
+  }
+
+
+  // Табы
+
+  function switchTabs() {
+    function hideTabContent(a) {
+      for (var i = a; i < tabContents.length; i++) {
+        tabContents[i].classList.add('services__hide');
+        tabContents[i].classList.remove('services__show');
+
+        tabs[i].classList.remove('services__tab-item--active');
+      }
+    }
+
+    hideTabContent(1);
+
+    function showTabContent(b) {
+      if (tabContents[b].classList.contains('services__hide')) {
+        tabContents[b].classList.remove('services__hide');
+        tabContents[b].classList.add('services__show');
+        tabs[b].classList.add('services__tab-item--active');
+      }
+    }
+
+    tabs.forEach(function (tab, i) {
+      tab.addEventListener('click', function () {
+        hideTabContent(0);
+        showTabContent(i);
+      });
+    });
+  }
+
+  var sliderServices = new Swiper('.services', {
+    breakpoints: {
+      320: {
+        init: true,
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+          bulletActiveClass: 'services__active-bullet'
+        }
+      },
+      1024: {
+        init: false
+      }
+    }
   });
+
+  function changeServicePage() {
+    if (window.matchMedia('(min-width: 1024px)').matches) {
+      sliderServices.destroy(false);
+      switchTabs();
+    } else if (window.matchMedia('(max-width: 1024px)').matches) {
+      sliderServices.init();
+    }
+  }
+
+  if (servicesPage) {
+    changeServicePage();
+    window.addEventListener('resize', changeServicePage);
+  }
+
 
 });
